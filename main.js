450aed33ca94d30a16d217a6ab0131ca36ab9bbd
JostMK <jost.klink@gmail.com>
2021-05-20 18:08:39 +0200

    Js Timer
    added timer functionality in js

---
+++ main.js
@@ -0,0 +1,31 @@
+//const revealDate = new Date(Date.UTC(2021, 4, 23, 22));
+
+const revealDate = new Date(Date.now());
+revealDate.setMinutes(5);
+
+const contentElement = document.getElementById('content');
+
+console.log(revealDate.toUTCString());
+
+var intervalId = setInterval(updateRemainingTime, 1000);
+
+function updateRemainingTime() {
+    contentElement.innerText = getRemainingTimeString();
+}
+
+function getRemainingTimeString() {
+    const millis = revealDate.valueOf() - Date.now();
+
+    if (millis <= 0) {
+        return getTheme();
+    } else {
+        const hours = Math.floor(millis / (1000 * 60 * 60))
+        const minutes = Math.floor(millis % (1000 * 60 * 60) / (1000 * 60))
+        const seconds = Math.floor(millis % (1000 * 60) / (1000))
+        return `${hours}h ${minutes}m ${seconds}s`;
+    }
+}
+
+function getTheme() {
+    return 'Theme:\nSuper Nice Theme';
+}
